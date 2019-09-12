b21064fce55fe1ee02a2924679e0752caa72e051
Ghina Yashar <ghina.yashar@rea-group.com>
2019-09-12 17:24:53 +1000

    Netlify build fail fix attempt

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'gatsby-link'
+import Logo from './Logo'
+import styled from 'styled-components'
+
+const isActive = className => ({ isCurrent }) => ({
+    className: `${className} ${isCurrent ? 'active' : ''}`
+});
+
+const ExactNavLink = ({ className, ...props }) => (
+    <Link getProps={isActive(className)} {...props} />
+);
+
+const NavLink = styled(ExactNavLink)`
+    color: white;
+    text-decoration: none;
+    
+    &.active {
+        text-shadow:1px 1px 12px #fff, 1px 1px 15px #ccc;
+    }
+    
+    &:not(.active):hover {
+        text-decoration-line: underline;
+    }
+`;
+
+const NavBar = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 50%;
+`;
+
+const Header = () => (
+    <NavBar>
+        <Logo />
+        <NavLink to={'/'}> Home </NavLink>
+        <NavLink to={'/about'}>About</NavLink>
+        <NavLink to={'/contact'}>Contact</NavLink>
+    </NavBar>
+)
+
+export default Header
