b47fde9170dbe53d9f1253f67d15d5969ee7be86
Ghina Yashar <ghina.yashar@rea-group.com>
2019-09-13 16:50:30 +1000

    Fixed css issue caused by gatsby build and moved from npm to yarn

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        title: `Ghina Yashar`,
+    },
+    plugins: [
+        `gatsby-plugin-styled-components`,
+        `gatsby-plugin-offline`,
+    ],
+};
