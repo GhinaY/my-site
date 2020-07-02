c7680e7841ec327dac45963bf51120d27649a882
Ghina Yashar <ghina.yashar@rea-group.com>
2020-07-02 21:53:55 +1000

    Added styled welcome block

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactMarkdown from "react-markdown";
 import Layout from '../components/Layout/Layout';
-
-const text = `# About me
-hi
-my name
-_is_
-## ghiona
-hello`;
+import profilePic from '../../assets/profilePicture.jpg';
+import '../styles/home.scss';
 
 export default () => (
     <Layout>
-        <div className='gradientLine' />
-        <ReactMarkdown source={text} />
+        <div className='welcomeBlock'>
+            <div className='gradientLine' />
+            <div className='welcomeContents'>
+                <div className='welcomeText'>
+                    <p className='welcomeSmall'>Hi! I'm</p>
+                    <h1 className='welcomeLarge'>G H I N A</h1>
+                    <p className='welcomeSmall'>Developer | Plant enthusiast | Friend :)</p>
+                    <p className='welcomeMedium'>Welcome to my site!</p>
+                </div>
+                <img className='profilePicture' src={profilePic} alt='profile' />
+            </div>
+            <div className='gradientLine' />
+        </div>
+        {/* <ReactMarkdown source={text} /> */}
     </Layout>
 );
