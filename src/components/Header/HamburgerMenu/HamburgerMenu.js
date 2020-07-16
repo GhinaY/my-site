4b0e4f2001cf9029833900fb45665de29dcf47b9
Ghina Yashar <ghina.yashar@rea-group.com>
2020-07-16 17:28:46 +1000

    Mobile styling fixes

--- src/components/Header/HamburgerMenu/HamburgerMenu.js
+++ src/components/Header/HamburgerMenu/HamburgerMenu.js
@@ -1,24 +1,51 @@
 import React, { useState } from 'react';
 import classNames from "classnames";
 import { CollapseReverse } from 'react-burgers';
+import { MenuItem } from '../Header';
 import "./HamburgerMenu.scss"
 
-const HamburgerMenu = ({ children }) => {
+const menuItems = {
+    About: -100,
+    Skills: 50,
+    Resume: 50,
+    Contact: 50
+};
+
+const HamburgerMenu = () => {
+    if (typeof window !== 'undefined') {
+        document.querySelectorAll('.hamburgerMenuItem').forEach(item => {
+            item.addEventListener('click', event => {
+                toggle()
+            })
+        })
+    };
+
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
+
     let activeSelector = (classname) => {
         return classNames({ 
             [classname]: true, 
             [`${classname}-active`]: isOpen,
         }); 
-    }
+    };
+
+    const menuList = Object.keys(menuItems).map(function(key) {
+        return <MenuItem
+                    subClassName='hamburgerMenuItem'
+                    key={key}
+                    itemName={key}
+                    offset={menuItems[key]}
+                />
+    });
+
     return (
         <>
             <div className={activeSelector("blurBackdrop")} />
             <div className="menuWrapper">
                 <CollapseReverse className={activeSelector("menuToggleButton")} active={isOpen} onClick={toggle} />
                 <ul className={activeSelector("menuList")}>
-                    {children.map((item, i) => (
+                    {menuList.map((item, i) => (
                         <li key={i}>{item}</li>
                     ))}
                 </ul>
--- src/components/Header/MobileHeader/MobileHeader.js
+++ src/components/Header/MobileHeader/MobileHeader.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import { ClickableLogo } from '../../Logo/Logo';
-import { MenuItem } from '../Header';
 import './MobileHeader.scss';
 
-const menuItems = {
-    About: -100,
-    Skills: 50,
-    Resume: 50,
-    Contact: 50
-};
-
 export default () => {
-
-    const menuList = Object.keys(menuItems).map(function(key) {
-        return <MenuItem subClassName='mobileMenuItem' key={key} itemName={key} offset={menuItems[key]} />
-    });
-
     return (
     <div className="mobileHeader">
         <ClickableLogo />
-        <HamburgerMenu>
-            {menuList}
-        </HamburgerMenu>
+        <HamburgerMenu />
     </div>
 )};
